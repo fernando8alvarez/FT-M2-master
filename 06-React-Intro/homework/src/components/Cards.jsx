1f49f68fde8da43eb37ae2d6f3ceecdc01a90465
Luis Fernando Alvarez <fernando_0694@hotmail.com>
2022-08-09 11:23:01 -0500

    modificacion

--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -14,6 +14,7 @@ export default function Cards({ cities }) {
         name={city.name}
         img={city.weather[0].icon}
         onClose={() => alert(city.name)}
+        key={city.name}
       />;
     })}
 
