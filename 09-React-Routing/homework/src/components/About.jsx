f384dba55cac8d7ae54a2346646543a1148f6b3e
Luis Fernando Alvarez <fernando_0694@hotmail.com>
2022-08-11 20:12:57 -0500

    Homework 9 - React Routing

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,8 @@
+
+export default function About(){
+    return(
+        <div>
+            <h4>Aca va la informacion sobre mi proyecto</h4>
+        </div>
+    )
+}
--- 09-React-Routing/homework/src/containers/App.js
+++ 09-React-Routing/homework/src/containers/App.js
@@ -3,8 +3,12 @@ import React, { useState } from 'react';
 import './App.css';
 import Nav from '../components/Nav.jsx';
 import Cards from '../components/Cards.jsx';
+import About from '../components/About.jsx';
+import Ciudad from '../components/Ciudad'
+import { Route, Switch } from 'react-router-dom';
 
-const apiKey = 'Aqui va la API key que creaste';
+
+const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -16,7 +20,7 @@ function App() {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
       .then(r => r.json())
       .then((recurso) => {
-        if(recurso.main !== undefined){
+        if (recurso.main !== undefined) {
           const ciudad = {
             min: Math.round(recurso.main.temp_min),
             max: Math.round(recurso.main.temp_max),
@@ -36,24 +40,44 @@ function App() {
         }
       });
   }
-  function onFilter(ciudadId) {
-    let ciudad = cities.filter(c => c.id === parseInt(ciudadId));
-    if(ciudad.length > 0) {
-        return ciudad[0];
+  function onFilter(ciudadId) {//cities = [{id:22},{id:1},{id:45}]
+    let ciudad = cities.filter(c => c.id === parseInt(ciudadId));//[{id:45}]
+    if (ciudad.length > 0) {
+      return ciudad[0];
     } else {
-        return null;
+      return null;
     }
+    
   }
+
   return (
     <div className="App">
-      <Nav onSearch={onSearch}/>
-      <div>
-        <Cards
-          cities={cities}
-          onClose={onClose}
+
+      <Route path='/'>
+        <Nav onSearch={onSearch} />
+      </Route>
+
+      <Switch>
+
+        <Route exact path='/'>
+          <hr />
+          <Cards
+            cities={cities}
+            onClose={onClose}
+          />
+          <hr />
+        </Route>
+
+        <Route
+          exact path='/ciudad/:ciudadId'
+          render={({ match }) => <Ciudad city={onFilter(match.params.ciudadId)} />}
         />
-      </div>
-      <hr />
+        <Route exact path='/about'>
+          <About />
+        </Route>
+      </Switch>
+
+
     </div>
   );
 }
