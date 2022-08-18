3e6c8a8855e486befcca3c14bed32456f9d1def0
Luis Fernando Alvarez <fernando_0694@hotmail.com>
2022-08-17 20:54:27 -0500

    modificacion

--- 08-React-Estado-LifeCycle/homework/src/components/Card.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Card.jsx
@@ -12,11 +12,11 @@ export default function Card ({min, max, name, img, onClose, id}) {
           <div className="row">
             <div className="col-sm-4 col-md-4 col-lg-4">
               <p>Min</p>
-              <p>{min}°</p>
+              <p>{min}°C</p>
             </div>
             <div className="col-sm-4 col-md-4 col-lg-4">
               <p>Max</p>
-              <p>{max}°</p>
+              <p>{max}°C</p>
             </div>
             <div className="col-sm-4 col-md-4 col-lg-4">
               <img className="iconoClima" src={"http://openweathermap.org/img/wn/"+img+"@2x.png"} width="80" height="80" alt="img not found" />
